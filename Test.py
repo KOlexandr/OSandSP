46f66b8dc2ba165a4e0ad3bfcc9c009e8374e631
Olexandr <kolexandr.13@gmail.com>
2014-04-02 21:05:47 +0300

    Initial commit

---
+++ Test.py
@@ -0,0 +1,34 @@
+import ctypes
+import random
+from Tools.Scripts.ftpmirror import raw_input
+
+__author__ = 'Olexandr'
+
+
+def create_array(size, min_val, max_val):
+    return [random.randint(min_val, max_val) for _ in range(size)]
+
+
+def get_data_for_creating_array():
+    size = int(raw_input("Please, input size of array: "))
+    while size < 1:
+        size = int(raw_input("size should be more than 0: "))
+    min_val = int(raw_input("Please, min value in array: "))
+    max_val = int(raw_input("Please, max value in array: "))
+
+    return size, min(min_val, max_val), max(min_val, max_val)
+
+
+def main():
+    size, min_val, max_val = get_data_for_creating_array()
+    array = create_array(size, min_val, max_val)
+
+    dll = ctypes.CDLL("Lab4.dll")
+    array_test = (ctypes.c_int * size)()
+    for i in range(size):
+        array_test[i] = array[i]
+    dll.caseAndSort(array_test, ctypes.c_int(size), dll.readSortNumber())
+    dll.printArray(array_test, ctypes.c_int(size))
+
+while True:
+    main()
