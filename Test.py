80d147289d6f8de58d0e961483d978d84576b2e8
Olexandr <kolexandr.13@gmail.com>
2014-04-02 21:27:50 +0300

    prints

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+import math
 import ctypes
 import random
 from Tools.Scripts.ftpmirror import raw_input
@@ -13,8 +14,8 @@ def get_data_for_creating_array():
     size = int(raw_input("Please, input size of array: "))
     while size < 1:
         size = int(raw_input("size should be more than 0: "))
-    min_val = int(raw_input("Please, min value in array: "))
-    max_val = int(raw_input("Please, max value in array: "))
+    min_val = math.fabs(int(raw_input("Please, min value in array: ")))
+    max_val = math.fabs(int(raw_input("Please, max value in array: ")))
 
     return size, min(min_val, max_val), max(min_val, max_val)
 
@@ -27,7 +28,11 @@ def main():
     array_test = (ctypes.c_int * size)()
     for i in range(size):
         array_test[i] = array[i]
+
+    print("Original array:")
+    dll.printArray(array_test, ctypes.c_int(size))
     dll.caseAndSort(array_test, ctypes.c_int(size), dll.readSortNumber())
+    print("Sorted array:")
     dll.printArray(array_test, ctypes.c_int(size))
 
 while True:
